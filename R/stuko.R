951c8c57323b36ae9da6d95522170dd26697de4c
Sebastian Kranz <sebastian.kranz@uni-ulm.de>
2017-11-05 06:00:15 +0100

    updates

---
+++ R/stuko.R
@@ -0,0 +1,9 @@
+stuko.examples = function() {
+
+
+  main.dir = "D:/libraries/stuko/ulm"
+  schema.dir = file.path(find.package("stuko"),"inst","schema")
+  db.dir = file.path(main.dir, "db")
+
+  dbmisc::dbCreateSQLiteFromSchema(schema.file = "stukodb.yaml",schema.dir = schema.dir, db.dir=db.dir)
+}
