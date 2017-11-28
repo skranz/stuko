698ecbcfeea3089f9de781ccc6ece60567380eb9
Sebastian Kranz <sebastian.kranz@uni-ulm.de>
2017-11-28 22:09:27 +0100

    updates

--- R/lehrangebot_report.R
+++ R/lehrangebot_report.R
@@ -6,7 +6,122 @@ examples.lehrangebot.report = function() {
   db = get.stukodb()
 
   semester = 160
-  copy.modules.to.semester(175, 160)
+  #copy.modules.to.semester(175, 160)
+
+  kurse = load.kurse.for.lehrangebot(semester=semester, db=db)
+
+
+  tpl.file = "lehrangebot_tpl.docx"
+  library(officer)
+  library(flextable)
+
+  doc = read_docx(tpl.file)
+  #ds = docx_summary(doc)
+
+  # Change bookmarks
+  doc = doc %>%
+    body_replace_at("sem_label",paste0(sem_label," ")) %>%
+    body_replace_at("date_label", date_label)
+
+
+  add.ft = function(key, dat, show.sp=FALSE) {
+    if (NROW(dat)==0) return(doc)
+    ft = kurse.lehrangebot.word.table(dat, show.sp)
+    doc %>%
+      cursor_bookmark(key) %>%
+      body_add_flextable(ft,align="left", pos = "on")
+  }
+
+  kurse$sp[is.na(kurse$sp)] = ""
+  vorl = filter(kurse, kursform %in% c("vu","v"))
+
+  key = "wiwi_ba_pflicht"
+  dat  = filter(vorl,ba_pflicht)
+  doc = add.ft(key,dat, show.sp=FALSE)
+
+  key = "wiwi_ba_wp"
+  dat  = filter(vorl,ba_wp & !bama_wp)
+  doc = add.ft(key,dat, show.sp=TRUE)
+
+
+  key = "wiwi_bama_wp"
+  dat  = filter(vorl,bama_wp)
+  doc = add.ft(key,dat, show.sp=TRUE)
+
+  key = "wiwi_ma_wp"
+  dat  = filter(vorl,ma_wp & !bama_wp)
+  doc = add.ft(key,dat, show.sp=TRUE)
+
+  key = "kein_modul"
+  nomo = anti_join(kurse, kumo, by="kursid")
+  doc = add.ft(key,nomo, show.sp=FALSE)
+
+  print(doc, target = "lehrangebot.docx")
+}
+
+cursor_bookmark_or_stay = function(x,id) {
+  res = try(cursor_bookmark(x,id),silent = TRUE)
+  if (is(res,"try-error")) {
+    return(x)
+  }
+  res
+}
+
+kurse.lehrangebot.word.table = function(dat, show.sp=FALSE) {
+  restore.point("kurse.lehrangebot.word.table")
+  df = adapt.kurse.for.lehrangebot(dat, show.sp=show.sp)
+
+  ft <- regulartable(data = df) %>% autofit() %>%
+    bold(part = "header") %>% bold(j=3) %>%
+    fontsize(size=12,part="all") %>%
+    align( align = "left", part = "all" ) %>%
+    align(align="center",j = 2, part="all") %>%
+    border(border=fp_border(style="none"), part="body") %>%
+    border(border=fp_border(style="none"), border.bottom = fp_border(style="solid", width=1), part="header") %>%
+    padding( padding.top = 2, padding.bottom=0, part = "body" ) %>%
+    rotate(align = "top", part = "body")
+
+
+  if (show.sp) {
+    ft = ft %>% bold(j=4) %>%
+      width(1,2.75) %>% width(2,0.7) %>% width(3,2) %>% width(4,1.5)
+  } else {
+    ft = ft %>% width(1,3.5) %>% width(2,1) %>% width(3,3)
+  }
+
+  ft
+}
+
+
+adapt.kurse.for.lehrangebot = function(df, show.sp=FALSE) {
+  li = lapply(1:NROW(df), function(row) {
+    res = adapt.kurs.for.lehrangebot(df[row,], show.sp=show.sp)
+  })
+  bind_rows(li)
+}
+
+adapt.kurs.for.lehrangebot = function(kurs, show.sp=FALSE) {
+  restore.point("adapt.kurs.for.lehrangebot")
+  kurs$Vorlesung = kurs$kursname
+
+  res = select(kurs, Vorlesung, SWS=sws_kurs, Dozent=dozent, Schwerpunkt=sp)
+  has_u = kurs$kursform %in% "vu"
+  if (has_u) {
+    if (is.na(kurs$ul) | is.true(kurs$ul==""))
+      kurs$ul = "NN"
+    res_u = transmute(kurs, Vorlesung="  - Übung", SWS=sws_uebung, Dozent=ul, Schwerpunkt="")
+    res = rbind(res,res_u)
+  }
+
+  res = rbind(res, data.frame(Vorlesung="",SWS="", Dozent="", Schwerpunkt=""))
+
+  if (!show.sp) res = res[,1:3]
+  res
+
+}
+
+load.kurse.for.lehrangebot = function(semester, db=get.stukodb()) {
+  restore.point("load.kurse.for.lehrangebot")
 
   kurse = dbGet(db,"kurs",params = list(semester=semester), schemas=stukodb.schemas())
 
@@ -25,7 +140,7 @@ examples.lehrangebot.report = function() {
   ba.ids = kust$kursid[kust$studiengang=="WiWi_BA"]
   ma.ids = kust$kursid[kust$studiengang=="WiWi_MA"]
   nuf.ids = kust$kursid[kust$studiengang=="WiWi_NUF"]
-  nuf.wp.ids = kuzu$kursid[str.starts.with(kuzu$zuordnung, "NUF WP")]
+  nuf.pflicht.ids = kuzu$kursid[str.starts.with(kuzu$zuordnung, "NUF Pflicht")]
 
   kurse$ba_wp = kurse$kursid %in% intersect(ba.ids, wp.ids)
   kurse$ba_pflicht = kurse$kursid %in% setdiff(ba.ids, wp.ids)
@@ -34,66 +149,47 @@ examples.lehrangebot.report = function() {
 
   kurse$nuf_wp = kurse$kursid %in% setdiff(nuf.ids, nuf.pflicht.ids)
   kurse$nuf_pflicht = kurse$kursid %in% nuf.pflicht.ids
+  kurse$bama_wp = kurse$ba_wp & kurse$ma_wp
+
 
   # Schwerpunkt via paste und dann per left_join an kurse matchen
   kusp = inner_join(kumo, mosp, by=c("semester","modulid"))
+  kusp = left_join(kusp,select(kurse, kursid, bama_wp), by="kursid") %>%
+    mutate(sp_bama=substring(schwerpunkt,1,2))
 
+  # Schwerpunkte von reinen Bachelor oder reinen Mastervorlesungen
+  kusp1 = kusp %>%
+    filter(!bama_wp) %>%
+    group_by(kursid) %>%
+    summarize(sp = paste0(unique(substring(schwerpunkt,4)), collapse=", ")) %>%
+    select(kursid, sp)
 
+  # Schwerpunkte von Kursen fuer Master und Bachelor
+  kusp2 = kusp %>%
+    filter(bama_wp) %>%
+    group_by(kursid,sp_bama) %>%
+    summarize(sp = paste0(unique(substring(schwerpunkt,4)), collapse=", ")) %>%
+    ungroup() %>%
+    tidyr::spread(key=sp_bama, value=sp) %>%
+    mutate(sp = paste0("BA:", BA,"\nMA:",MA)) %>%
+    select(kursid, sp)
 
-  tpl.file = "lehrangebot_tpl.docx"
-  library(officer)
-  library(flextable)
+  kusp_sum = rbind(kusp1, kusp2)
 
-  doc = read_docx(tpl.file)
-  #ds = docx_summary(doc)
+  kurse = left_join(kurse, kusp_sum, by="kursid")
 
-  # Change bookmarks
-  doc = doc %>%
-    body_replace_at("sem_label",paste0(sem_label," ")) %>%
-    body_replace_at("date_label", date_label)
+  # Dozenten
+  kudo = kupe %>%
+    filter(rolle %in% c("do","dk","ul")) %>%
+    mutate(rolle = ifelse(rolle=="ul", "ul", "do")) %>%
+    group_by(kursid, rolle) %>%
+    summarize(dozent = paste0(unique(paste0(substring(vorname,1,1), ". ", nachname)), collapse=", ")) %>%
+    ungroup() %>%
+    tidyr::spread(key=rolle, value=dozent) %>%
+    rename(dozent=do)
 
-
-  df = adapt.kurse.for.table(kurse[1:15,])
-
-  ft <- regulartable(data = df) %>%
-    #theme_vanilla() %>%
-    bold(part = "header") %>% bold(j=3) %>%
-    fontsize(size=12,part="all") %>%
-    align( align = "left", part = "all" ) %>%
-    align(align="center",j = 2, part="all") %>%
-    border(border=fp_border(style="none"), part="body") %>%
-    border(border=fp_border(style="none"), border.bottom = fp_border(style="solid", width=1), part="header") %>%
-    autofit() %>% width(1,4) %>% width(2,1) %>% width(3,3)
-
-  doc = doc %>%
-    cursor_bookmark("wiwi_ba_pflicht") %>%
-    body_add_flextable(ft,align="left", pos = "on")
-  print(doc, target = "lehrangebot.docx")
+  kurse = left_join(kurse, kudo, by="kursid")
+  kurse = kurse[!duplicated(kurse),]
+  kurse
 }
 
-cursor_bookmark_or_stay = function(x,id) {
-  res = try(cursor_bookmark(x,id),silent = TRUE)
-  if (is(res,"try-error")) {
-    return(x)
-  }
-  res
-}
-
-adapt.kurse.for.table = function(df) {
-  li = lapply(1:NROW(df), function(row) {
-    res = adapt.kurs.for.table(df[row,])
-  })
-  bind_rows(li)
-}
-
-adapt.kurs.for.table = function(kurs) {
-  restore.point("adapt.kurs.for.table")
-  doz = filter(kupe, kursid==kurs$kursid)
-
-  kurs$dozenten = paste0(unique(paste0(substring(doz$vorname,1,1), ". ", doz$nachname)), collapse=", ")
-  kurs$Vorlesung = paste0(kurs$kursname,"\n")
-  res = select(kurs, Vorlesung, SWS=sws_kurs, Dozent=dozenten)
-  res = rbind(res, data.frame(Vorlesung=" ",SWS="", Dozent=""))
-
-
-}
